79e7454d15f3f68c65231dc8c36db3f59daa16e4
Stuff1Do <207966845+Stuff1Do@users.noreply.github.com>
2025-06-02 18:55:30 +0800

    add timer logic, continue with short break and long break

--- script.js
+++ script.js
@@ -7,16 +7,48 @@ const mainTimer = document.querySelector('.pomodoro')
 const shortBreak = document.querySelector('.short-break')
 const longBreak = document.querySelector('.long-break');
 
+const setMin ="25";
+const setSec = "00"
+let maxTimer = 1500;
 
+let actualTimer = setMin + ":" + setSec;
+function calculateTimer(time){
+    
+    let timeInMinutes = Math.floor(time / 60);
+    let timeInSeconds = time % 60;
+    
+    let formattedMinutes = String(timeInMinutes).padStart(2, "0");
+    let formattedSeconds = String(timeInSeconds).padStart(2, "0");
+    console.log(formattedMinutes);
+    console.log(formattedSeconds);
+    actualTimer = `${formattedMinutes}:${formattedSeconds}`;
+}
 let clickCtr = 0;
+
+
+let timeInterval = null;
 startButton.addEventListener('click', ()=>{
     clickCtr++;
+    
     if(clickCtr % 2== 0){
         startButton.textContent = "start"; 
+        clearInterval(timeInterval);
     }else{
         startButton.textContent = "pause";
-    }
-})
+        timeInterval = setInterval(() =>{
+                let time = maxTimer--;
+                calculateTimer(time);
+                
+                timer.textContent = actualTimer;
+                
+                if(maxTimer < 0){
+                    clearInterval(timeInterval);
+                }
+            }, 1000)
+        }
+        
+    })
+  
 
 mainTimer.addEventListener('click', ()=>{
     timer.textContent = "25:00";
@@ -24,6 +56,7 @@ mainTimer.addEventListener('click', ()=>{
 })
 
 shortBreak.addEventListener('click', ()=>{
+    clearInterval(timeInterval);
     timer.textContent = "5:00";
     stateColorController(shortBreak, mainTimer, longBreak)
 })
