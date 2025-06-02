1ee698ec0ad9fdb045c8c30cfe12c239c1cd8752
Stuff1Do <207966845+Stuff1Do@users.noreply.github.com>
2025-06-03 07:42:07 +0800

    fix stall bug where timer stalls for a second at the start

--- script.js
+++ script.js
@@ -5,38 +5,34 @@ const timer = document.querySelector('.timer')
 const reset = document.querySelector('.reset')
 const mainTimer = document.querySelector('.pomodoro')
 const shortBreak = document.querySelector('.short-break')
-const longBreak = document.querySelector('.long-break');
+const longBreak = document.querySelector('.long-break')
 
-const setMin ="25";
-const setSec = "00"
 let maxTimer = 1500;
+let clickCtr = 0;
+let timeInterval = null;
+let actualTimer;
 
-let actualTimer = setMin + ":" + setSec;
 function calculateTimer(time){
-    
     let timeInMinutes = Math.floor(time / 60);
     let timeInSeconds = time % 60;
     
+    //format single digit numbers to be padded with 0 at the start
     let formattedMinutes = String(timeInMinutes).padStart(2, "0");
     let formattedSeconds = String(timeInSeconds).padStart(2, "0");
-    console.log(formattedMinutes);
-    console.log(formattedSeconds);
-    actualTimer = `${formattedMinutes}:${formattedSeconds}`;
+
+    actualTimer = `${formattedMinutes}:${formattedSeconds}`; 
 }
-let clickCtr = 0;
-
-
-let timeInterval = null;
-startButton.addEventListener('click', ()=>{
+function showTimer(){
     clickCtr++;
     
     if(clickCtr % 2== 0){
         startButton.textContent = "start"; 
-        clearInterval(timeInterval);
+        clearInterval(timeInterval); //stops the timer if the user clicks pause
     }else{
         startButton.textContent = "pause";
         timeInterval = setInterval(() =>{
-                let time = maxTimer--;
+                let time = --maxTimer;
+                
                 calculateTimer(time);
                 
                 timer.textContent = actualTimer;
@@ -45,24 +41,31 @@ startButton.addEventListener('click', ()=>{
                     clearInterval(timeInterval);
                 }
             }, 1000)
+            
         }
         
-    })
+ }
   
+
+startButton.addEventListener('click', showTimer)
 
 mainTimer.addEventListener('click', ()=>{
     timer.textContent = "25:00";
+    clearInterval(timeInterval);
     stateColorController(mainTimer, shortBreak, longBreak)
 })
 
 shortBreak.addEventListener('click', ()=>{
     clearInterval(timeInterval);
     timer.textContent = "5:00";
+    startButton.textContent ="start";
     stateColorController(shortBreak, mainTimer, longBreak)
 })
 
-longBreak.addEventListener('click', e=>{
+longBreak.addEventListener('click', ()=>{
+    clearInterval(timeInterval);
     timer.textContent = "10:00";
+    startButton.textContent ="start";
     stateColorController(longBreak, mainTimer, shortBreak)
 })
 
