9cb64a82f55a464b561c6fd3b61912ce596a8cc6
Stuff1Do <207966845+Stuff1Do@users.noreply.github.com>
2025-06-05 11:35:47 +0800

    add timer setting functionality, missing timer > hour functionality and settings pages

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 const startButton = document.querySelector('.start')
 const timer = document.querySelector('.timer')
 const reset = document.querySelector('.reset')
@@ -7,17 +5,27 @@ const mainTimer = document.querySelector('.pomodoro')
 const shortBreak = document.querySelector('.short-break')
 const longBreak = document.querySelector('.long-break')
 const resetContainer = document.querySelector('.reset-container')
-const modal = document.querySelector('#modal');
+const modal = document.querySelector('#modal')
 const settingsContainer = document.querySelector('.settings-container')
 const closeModal = document.querySelector('.close-modal')
-const inputPomodoro = document.querySelector('#pomo-minutes');
+const inputPomodoro = document.querySelector('#pomo-minutes')
 const inputShortBreak = document.querySelector('#short-minutes')
-const inputLongBreak = document.querySelector('#long-minutes');
-const submit = document.querySelector('.save-changes');
-
+const inputLongBreak = document.querySelector('#long-minutes')
+const submit = document.querySelector('.save-changes')
+const alertInvalid = document.querySelector('.invalid-input')
 
 const MAXIMUM_TIMER_DIGITS = 2;
 const SECONDS_TO_MIN_FACTOR = 60;
+
+const defaultPomodoroSeconds = 1500;
+const defaultShortBreakSeconds = 300;
+const defaultLongBreakSeconds = 600;
+const defaultPomodoroString = "25:00"
+const defaultShortBreakString = "5:00";
+const defaultLongBreakString = "10:00";
+const defaultInputFieldPomodoro = 25;
+const defaultInputFieldShort = 5;
+const defaultInputFieldLong =10;
 
 let userInputPomodoro = inputPomodoro.value;
 let userInputShortBreak = inputShortBreak.value;
@@ -26,9 +34,9 @@ let userInputLongBreak = inputLongBreak.value;
 let userInputPomodoroSeconds = userInputPomodoro * 60;
 let userInputShortBreakSeconds = userInputShortBreak * 60;
 let userInputLongBreakSeconds = userInputLongBreak * 60;  
-let pomodoroMinuteString = "25:00";
-let shortBreakMinuteString = "5:00";
-let longBreakMinuteString = "10:00";
+let pomodoroMinuteString = defaultPomodoroString;
+let shortBreakMinuteString = defaultShortBreakString;
+let longBreakMinuteString = defaultLongBreakString;
 
 let shortBreakSeconds = userInputShortBreakSeconds;
 let longBreakSeconds = userInputLongBreakSeconds;
@@ -36,22 +44,28 @@ let pomodoroSeconds = userInputPomodoroSeconds;
 let pomodoroMinutes = pomodoroMinuteString;
 let shortBreakMinutes = shortBreakMinuteString;
 let longBreakMinutes = longBreakMinuteString;
-let maxTimer = pomodoroSeconds;
+let maxTimer = userInputPomodoroSeconds;
 let clickCtr = 0;
 let timeInterval = null;
 let currentState = "main_timer";
 timer.textContent = pomodoroMinutes;
 
+let inputFieldPomo = defaultInputFieldPomodoro;
+let inputFieldShort = defaultInputFieldShort;
+let inputFieldLong = defaultInputFieldLong;
 
-
-function convertToString(num){
-    
+function convertToString(num){    
     return `${num}:00`;
 }
 
 submit.addEventListener('click', ()=>{
-     userInputPomodoro = inputPomodoro.value;
-     userInputShortBreak = inputShortBreak.value;
+    if(!inputPomodoro.value || !inputShortBreak.value || !inputLongBreak.value || inputPomodoro.value == 0 || 
+        inputShortBreak.value == 0 || inputLongBreak.value == 0){
+        alertInvalid.textContent = "invalid!"
+        alertInvalid.style.color = "red";
+    }else{
+        userInputPomodoro = inputPomodoro.value;
+      userInputShortBreak = inputShortBreak.value;
      userInputLongBreak = inputLongBreak.value;
     
     userInputPomodoroSeconds = userInputPomodoro * 60;
@@ -66,13 +80,35 @@ submit.addEventListener('click', ()=>{
     shortBreakMinutes =shortBreakMinuteString;
     longBreakMinutes = longBreakMinuteString;
     
-    timer.textContent = pomodoroMinutes;
+    if(currentState == "main_timer"){ 
+        stateTimer(pomodoroMinutes, userInputPomodoroSeconds);
+    }else if(currentState == "short_break"){
+        stateTimer(shortBreakMinutes, userInputShortBreakSeconds);
+    }else if(currentState == "long_break"){
+        stateTimer(longBreakMinutes, userInputLongBreakSeconds);
+    }
+    
     maxTimer = userInputPomodoroSeconds;
     shortBreakSeconds = userInputShortBreakSeconds;
     longBreakSeconds = userInputLongBreakSeconds;
+    
+    inputFieldPomo = userInputPomodoro;
+    inputFieldShort = userInputShortBreak;
+    inputFieldLong = userInputLongBreak;
+    alertInvalid.textContent = '';
     modal.close();
+    }
+
+     
 })
 
+closeModal.addEventListener('click', ()=>{
+    inputPomodoro.value = inputFieldPomo;
+    inputShortBreak.value = inputFieldShort;
+    inputLongBreak.value = inputFieldLong;
+   
+    modal.close();
+})
 
 function calculateTimer(time){
     let timeInMinutes = Math.floor(time / SECONDS_TO_MIN_FACTOR); 
@@ -85,13 +121,7 @@ function calculateTimer(time){
     return `${formattedMinutes}:${formattedSeconds}`; 
 }
 
-function stateTimer(e, minutes, seconds) {
-    clearInterval(timeInterval);
-    timer.textContent = minutes;        
-    startButton.textContent ="start";
-    maxTimer = seconds;
-    clickCtr = 0; //resets clickctr so its odd when start button is clicked again
-  }
+
 function stateColorController(currentTimer, timer1, timer2){
     currentTimer.style.background = "white";
     currentTimer.style.borderColor = "white";
@@ -132,15 +162,21 @@ startButton.addEventListener('click', () =>{
 document.addEventListener('click', function(e) { //this is so i can pass the event e
     if (e.target.closest('.reset-container')) {
         if(currentState == "main_timer"){ 
-            stateTimer(e, pomodoroMinutes, pomodoroSeconds);
+            stateTimer(pomodoroMinutes, userInputPomodoroSeconds);
         }else if(currentState == "short_break"){
-            stateTimer(e, shortBreakMinutes, shortBreakSeconds);
+            stateTimer(shortBreakMinutes, userInputPomodoroSeconds);
         }else if(currentState == "long_break"){
-            stateTimer(e, longBreakMinutes, longBreakSeconds)
+            stateTimer(longBreakMinutes, userInputPomodoroSeconds);
         }
     }
 });
-
+function stateTimer(minutes, seconds) {
+    clearInterval(timeInterval);
+    timer.textContent = minutes;        
+    startButton.textContent ="start";
+    maxTimer = seconds;
+    clickCtr = 0; //resets clickctr so its odd when start button is clicked again
+}
 
 document.addEventListener('click', (e)=>{
     if(e.target.closest('.settings-container')){
@@ -149,9 +185,7 @@ document.addEventListener('click', (e)=>{
     }
 })
 
-closeModal.addEventListener('click', ()=>{
-    modal.close();
-})
+
 
 
 mainTimer.addEventListener('click', ()=>{
@@ -159,7 +193,7 @@ mainTimer.addEventListener('click', ()=>{
     clearInterval(timeInterval);
     timer.textContent = pomodoroMinutes;
     startButton.textContent ="start";
-    maxTimer = pomodoroSeconds;
+    maxTimer = userInputPomodoroSeconds;
     currentState = "main_timer"
     stateColorController(mainTimer, shortBreak, longBreak)
 })
@@ -168,7 +202,7 @@ shortBreak.addEventListener('click', ()=>{
     clearInterval(timeInterval);
     timer.textContent = shortBreakMinutes;
     startButton.textContent ="start";
-    maxTimer = shortBreakSeconds; 
+    maxTimer = userInputShortBreakSeconds; 
     currentState ="short_break";
     stateColorController(shortBreak, mainTimer, longBreak)
 })
@@ -177,7 +211,7 @@ longBreak.addEventListener('click', ()=>{
     clearInterval(timeInterval);
     timer.textContent = longBreakMinutes;
     startButton.textContent ="start";
-    maxTimer = longBreakSeconds ;
+    maxTimer = userInputLongBreakSeconds ;
     currentState = "long_break";
     stateColorController(longBreak, mainTimer, shortBreak)
 })
